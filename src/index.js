3704371bf468488aadd7113597b7f421d822b676
mokshhhhh <mokshdhiman2004@gmail.com>
2025-06-09 22:59:29 +0530

    utils , api err, api res

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ dotenv.config({
 })
 
 DBconnect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO Db connection failed",err);
+})
 
 
 
