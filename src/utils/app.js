ea564b5c358bde722e44171ddbe6b581e7167fd0
mokshhhhh <mokshdhiman2004@gmail.com>
2025-06-12 23:01:43 +0530

    started w controllers

--- src/utils/app.js
+++ src/utils/app.js
@@ -10,4 +10,12 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,lmit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+//https://localhost:8000/api/v1/users/register
 export {app}
