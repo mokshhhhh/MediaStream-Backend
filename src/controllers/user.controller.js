7bb537240b0b26ce89e7597b37d2cf059720a787
mokshhhhh <mokshdhiman2004@gmail.com>
2025-07-19 19:12:53 +0530

    aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -262,7 +262,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -341,7 +341,86 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "cover image updated successfully"));
 });
 
+//aggregation pipeline
+const getUserChannel = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+  // {},{},{} ->diff stages in aggregate pipeline
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      //for no of subscribers
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      //for ppl ive (channel) subsribed
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subsribersCount: {
+          $size: "$subscribers",
+        },
 
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        
+        isSubscribed:{
+            $cond:{
+                if:{
+                    $in:[req.user?._id,"$subscribers.subscriber"]
+                },
+                then:true,
+                else:false
+                
+            }
+        }
+      },
+    },
+
+    {
+        $project:{
+            fullname:1,
+            email:1,
+            username:1,
+            subsribersCount:1,
+            channelsSubscribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1
+        }
+
+    }
+  ]);
+  
+
+  if(!channel?.length){
+    throw new ApiError(404,"channel doesn not exist")
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,channel[0],"user channel fetched successfully"))
+
+
+});
 
 export {
   registerUser,
@@ -353,4 +432,5 @@ export {
   updateAccountDetails,
   updateAvatar,
   updateCoverImage,
+  getUserChannel,
 };
